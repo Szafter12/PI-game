ef4f1ed00cf11e52affaf1527745b312b66ead4f
FractalCollider <wiktorus356@gmail.com>
2025-12-06 13:41:50 +0100

    Commit message

---
+++ src/Player.h
@@ -0,0 +1,73 @@
+#ifndef GAME_PI_PLAYER_H
+#define GAME_PI_PLAYER_H
+#include <iostream>
+
+
+class Player {
+    public:
+    sf::Texture texture[8] = {sf::Texture("../../Grafika/HeroBase.png")};
+    sf::Sprite sprite = sf::Sprite(texture[0]);
+    sf::Vector2f position;
+
+    Player(const char* texturePath,sf::Vector2f position) {
+        if (!texture[0].loadFromFile(texturePath)) std::cout<<"Error loading texture"<<std::endl;
+        this->position = position;
+        sprite.setTexture(texture[0]);
+        sprite.setPosition(position);
+    }
+    Player(sf::Vector2f position) {
+        texture[0].loadFromFile("../../Grafika/HeroIdleUp.png");
+        texture[1].loadFromFile("../../Grafika/HeroIdleUpR.png");
+        texture[2].loadFromFile("../../Grafika/HeroIdleR.png");
+        texture[3].loadFromFile("../../Grafika/HeroIdleDownR.png");
+        texture[4].loadFromFile("../../Grafika/HeroBase.png");
+        texture[5].loadFromFile("../../Grafika/HeroIdleDownL.png");
+        texture[6].loadFromFile("../../Grafika/HeroIdleL.png");
+        texture[7].loadFromFile("../../Grafika/HeroIdleUpL.png");
+        this->position = position;
+        sprite.setTexture(texture[0]);
+        sprite.setPosition(position);
+    }
+
+    void update(sf::RenderWindow &window) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::A)) {
+            sprite.move({-1,0});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::D)) {
+            sprite.move({1,0});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W)) {
+            sprite.move({0,-1});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S)) {
+            sprite.move({0,1});
+        }
+
+        position = sprite.getPosition();
+        sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+
+        if (mousePos.y > position.y &&
+            mousePos.y < (position.y+sprite.getTexture().getSize().y)) {
+            if (mousePos.x < position.x) sprite.setTexture(texture[6]);
+            else sprite.setTexture(texture[2]);
+        }
+        else if (mousePos.y < position.y) {
+            if (mousePos.x < position.x) sprite.setTexture(texture[7]);
+            else if (mousePos.x > position.x &&
+                mousePos.x < (position.x+sprite.getTexture().getSize().x)) sprite.setTexture(texture[0]);
+            else sprite.setTexture(texture[1]);
+        }
+        else {
+            if (mousePos.x < position.x) sprite.setTexture(texture[5]);
+            else if (mousePos.x > position.x &&
+                mousePos.x < (position.x+sprite.getTexture().getSize().x)) sprite.setTexture(texture[4]);
+            else sprite.setTexture(texture[3]);
+        }
+    }
+
+    void draw(sf::RenderWindow &window) {
+        window.draw(sprite);
+    }
+};
+
+#endif //GAME_PI_PLAYER_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
+#include "Player.h"
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Gierka PI");
     window.setFramerateLimit(144);
+
+    Player player({800,600});
+    sf::Texture texture("../../Grafika/map.png");
+    sf::Sprite obiekt(texture);
+    obiekt.setPosition({0,0});
+    sf::View view({player.position.x+16, player.position.y+16}, {400.f, 300.f});
+
 
     while (window.isOpen())
     {
@@ -16,6 +24,15 @@ int main()
         }
 
         window.clear();
+        window.draw(obiekt);
+        player.update(window);
+
+        view.setCenter({player.position.x+16, player.position.y+16});
+        window.setView(view);
+
+        player.draw(window);
+
+
         window.display();
     }
 }
